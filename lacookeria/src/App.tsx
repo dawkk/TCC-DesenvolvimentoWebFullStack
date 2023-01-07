92dbd8d555760a35c3e15b08562ef4425cd7ce0a
dawkk <gventuri14@gmail.com>
2023-01-07 06:47:33 -0300

    installs first dependencies

---
+++ lacookeria/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>}>
+
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
---
+++ lacookeria/src/pages/Home/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Home.module.scss'
+
+const Home = () => {
+  return (
+    <>
+    
+    
+    </>
+  )
+}
+
+export default Home
