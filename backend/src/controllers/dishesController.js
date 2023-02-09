be83ac7edf3a0a6784268db8bf743982986efb35
dawkk <gventuri14@gmail.com>
2023-02-09 19:03:10 -0300

    Added a new search method by type

--- backend/src/controllers/dishesController.js
+++ backend/src/controllers/dishesController.js
@@ -17,6 +17,7 @@ class DishController {
   static listDishById = (req, res) => {
     const id = req.params.id;
     dishes.findById(id)
+    /* Lembrar de deixar o nome do esquema em singular, por ex menus é o esquema porem abaixo usamos menu */
       .populate('menu', 'name')
       .exec((err, dishes) => {
         if(err) {
@@ -56,6 +57,18 @@ class DishController {
     dishes.findByIdAndDelete(id, (err) => {
       if(!err){
         res.status(200).send({message: `Prato ${id} removido com sucesso!`})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
+
+  static listDishByType = (req, res) => {
+    const type = req.query.type;
+
+    dishes.find({'type': type}, {}, (err, dishes) => {
+      if(!err){
+        res.status(200).send(dishes)
       } else {
         res.status(500).send({message: err.message})
       }
--- backend/src/routes/dishRoutes.js
+++ backend/src/routes/dishRoutes.js
@@ -3,8 +3,11 @@ import DishController from "../controllers/dishesController.js";
 
 const router = express.Router();
 
+/* quando projetar rotas precisamos colocar no topo da mais especifica para a menos especifica, ou teremos erros de código */
+
 router
   .get("/dishes", DishController.listDishes)
+  .get("/dishes/search", DishController.listDishByType)
   .get("/dishes/:id", DishController.listDishById)
   .post("/dishes", DishController.createDish)
   .put("/dishes/:id", DishController.updateDish)
