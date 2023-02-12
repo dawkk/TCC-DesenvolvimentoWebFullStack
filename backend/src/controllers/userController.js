5db0a76c095bdea8a086316a762cf88b7928be72
dawkk <gventuri14@gmail.com>
2023-02-12 12:58:12 -0300

    Added filter criteria for existing e-mail register and started encrypt on register

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,4 +1,5 @@
 import users from "../models/user.js"
+import * as bcrypt from 'bcrypt';
 
 class UserController {
 
@@ -6,48 +7,64 @@ class UserController {
     users.find()
       /* .populate('address') */
       .exec((err, users) => {
-        if(err) {
-          res.status(400).send({message: `${err.message} - Id de usuario não encontrado. `})
+        if (err) {
+          res.status(400).send({ message: `${err.message} - Id de usuario não encontrado. ` })
         } else {
           res.status(200).json(users)
         }
       }
-  )}
+      )
+  }
 
   static listUserById = (req, res) => {
     const id = req.params.id;
     users.findById(id)
-    /* Lembrar de deixar o nome do esquema em singular, por ex menus é o esquema porem abaixo usamos menu */
-     /*  .populate('menu', 'name') */
+      /* Lembrar de deixar o nome do esquema em singular, por ex menus é o esquema porem abaixo usamos menu */
+      /*  .populate('menu', 'name') */
+
       .exec((err, users) => {
-        if(err) {
-          res.status(400).send({message: `${err.message} - Id do usuario não encontrado. `})
+        if (err) {
+          res.status(400).send({ message: `${err.message} - Id do usuario não encontrado. ` })
         } else {
           res.status(200).send(users)
         }
+
       }
-    );
+      );
   }
 
-  static createUser = (req, res) => {
+  static createUser = async (req, res) => {
     /* para lembrar users aqui se refere ao schema/coleção que criamos no mongoose em models como referencia */
     let user = new users(req.body);
-    user.save((err) => {
-      if(err) {
-        res.status(500).send({message: `${err.message} - Falha ao cadastrar usuario.`})
-      } else {
-        res.status(201).send(user.toJSON())
-      }
-    })
+    const userExists = await users.findOne({ email: req.body.email });
+  
+
+    if (userExists) {
+      return res.status(422).send({ message: "Por favor, utilize outro e-mail!" });
+    } else {
+
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(req.body.password, salt);
+      user.password = passwordHash;
+      user.save((err) => {
+        if (err) {
+          res.status(500).send({ message: `${err.message} - Falha ao cadastrar usuario.` })
+        } else {
+          res.status(201).send(user.toJSON())
+        }
+      })
+
+    }
+
   }
 
   static updateUser = (req, res) => {
     const id = req.params.id;
-    users.findByIdAndUpdate(id, {$set: req.body}, (err) => {
-      if(!err){
-        res.status(200).send({message: 'usuario atualizado com sucesso!'})
+    users.findByIdAndUpdate(id, { $set: req.body }, (err) => {
+      if (!err) {
+        res.status(200).send({ message: 'usuario atualizado com sucesso!' })
       } else {
-        res.status(500).send({message: err.message})
+        res.status(500).send({ message: err.message })
       }
     })
   }
@@ -55,10 +72,10 @@ class UserController {
   static deleteUser = (req, res) => {
     const id = req.params.id;
     users.findByIdAndDelete(id, (err) => {
-      if(!err){
-        res.status(200).send({message: `usuario ${id} removido com sucesso!`})
+      if (!err) {
+        res.status(200).send({ message: `usuario ${id} removido com sucesso!` })
       } else {
-        res.status(500).send({message: err.message})
+        res.status(500).send({ message: err.message })
       }
     })
   }
@@ -66,11 +83,11 @@ class UserController {
   static listUserByEmail = (req, res) => {
     const email = req.query.email;
 
-    users.find({'email': email}, {}, (err, users) => {
-      if(!err){
+    users.find({ 'email': email }, {}, (err, users) => {
+      if (!err) {
         res.status(200).send(users)
       } else {
-        res.status(500).send({message: err.message})
+        res.status(500).send({ message: err.message })
       }
     })
   }
--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
   email: {type: String, required: true},
-  password:{type: String},
+  password:{type: String, required: true},
   /* address: {type: mongoose.Schema.Types.ObjectId, ref:'adresses', required: true}, */
   status: {type: Boolean}
 });
