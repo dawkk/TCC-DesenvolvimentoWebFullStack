be78c30afc99ad3b7b7a389c3cec9b9ef38749ff
dawkk <gventuri14@gmail.com>
2023-02-25 13:19:20 -0300

    Register page form updated with formik and yup for initial validations

--- lacookeria/src/pages/Authentication/Register/FormFields/index.tsx
+++ lacookeria/src/pages/Authentication/Register/FormFields/index.tsx
@@ -1,36 +1,50 @@
-import { Box, Button, Checkbox, Divider, FormControlLabel, Grid, InputLabel, Link, OutlinedInput, Paper, Stack, Typography } from '@mui/material';
+import { Button, FormHelperText, Grid, IconButton, InputAdornment, InputLabel, OutlinedInput, Stack, Typography } from '@mui/material';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 import * as Yup from 'yup';
 import { Formik } from 'formik';
+import { useState } from 'react';
 
 const FormFields = () => {
+  const [showPassword, setShowPassword] = useState(false);
+  const handleClickShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const celRegex = /([0-9]{2,3})?(\([0-9]{2}\))([0-9]{4,5})([0-9]{4})/;
+
   return (
     <>
       <Formik
         initialValues={{
           firstName: '',
           lastName: '',
+          cellphone: '',
           email: '',
           password: '',
-          city: '',
-          state: '',
-          neighborhood: '',
-          street: '',
-          number: '',
-          zipcode: '',
+          addressStreet: '',
+          addressNumber: '',
+          addressNeighborhood: '',
+          addressCity: '',
+          addressState: '',
+          addressZipcode: '',
+          addressExtra: '',
           submit: null
         }}
         validationSchema={Yup.object().shape({
-          firstName: Yup.string().max(255).required('Preenchimento do nome Obrigatório'),
-          lastName: Yup.string().max(255).required('Preenchimento do nome Obrigatório'),
-          email: Yup.string().email('Must be a valid email').max(255).required('Email is required'),
-          password: Yup.string().max(255).required('Preenchimento do nome Obrigatório'),
-          city: Yup.string().max(255).required('Preenchimento do nome Obrigatório'),
-          state: Yup.string().max(255).required('Preenchimento do nome Obrigatório'),
-          neighborhood: Yup.string().max(255).required('Preenchimento do nome Obrigatório'),
-          street: Yup.string().max(255).required('Preenchimento do nome Obrigatório'),
-          number: Yup.number().max(255).required('Preenchimento do nome Obrigatório'),
-          zipcode: Yup.number().max(255).required('Preenchimento do nome Obrigatório')
+          firstName: Yup.string().max(255).required('Nome Obrigatório'),
+          lastName: Yup.string().max(255).required('Sobrenome Obrigatório'),
+          cellphone: Yup.string().matches(celRegex, 'Este numero não é valido, o formato deveria ser (XX)XXXXXXXXX').required('Celular Obrigatório'),
+          email: Yup.string().email('Must be a valid email').max(255).required('Email Obrigatório'),
+          password: Yup.string().max(255).required('Senha Obrigatória'),
+          addressStreet: Yup.string().max(255),
+          addressNumber: Yup.number(),
+          addressNeighborhood: Yup.string().max(255),
+          addressCity: Yup.string().max(255),
+          addressState: Yup.string().max(255),
+          addressZipcode: Yup.number(),
+          addressExtra: Yup.string().max(255),
         })}
         onSubmit={async (values, { setErrors, setStatus, setSubmitting }) => {
           try {
@@ -39,7 +53,7 @@ const FormFields = () => {
           } catch (err) {
             console.error(err);
             setStatus({ success: false });
-            setErrors({ submit: err.message });
+            /* setErrors({ submit: err.message }); */
             setSubmitting(false);
           }
         }}
@@ -57,50 +71,62 @@ const FormFields = () => {
                 <Stack spacing={1}>
                   <InputLabel>Nome*</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="John"
+                    id='firstName'
+                    type='firstName'
+                    value={values.firstName}
+                    name='firstName'
+                    placeholder='Nome'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.firstName && errors.firstName)}
                   />
+                  {touched.firstName && errors.firstName && (
+                    <FormHelperText error id="helper-text-firstname-signup">
+                      {errors.firstName}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
 
               <Grid item xs={12} md={6}>
                 <Stack spacing={1}>
-                  <InputLabel>Sobrenome*</InputLabel>
+                  <InputLabel>Ultimo Sobrenome*</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="Doe"
+                    id='lastName'
+                    type='lastName'
+                    value={values.lastName}
+                    name='lastName'
+                    placeholder='Sobrenome'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.lastName && errors.lastName)}
                   />
+                  {touched.lastName && errors.lastName && (
+                    <FormHelperText error id="helper-text-lastname-signup">
+                      {errors.lastName}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
 
               <Grid item xs={12} md={6}>
                 <Stack spacing={1}>
-                  <InputLabel>Data de Nascimento</InputLabel>
+                  <InputLabel>Celular*</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="15/04/1995"
+                    id='cellphone'
+                    type='cellphone'
+                    value={values.cellphone}
+                    name='cellphone'
+                    placeholder='cellphone'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.cellphone && errors.cellphone)}
                   />
-                </Stack>
-              </Grid>
-
-              <Grid item xs={12} md={6}>
-                <Stack spacing={1}>
-                  <InputLabel>Celular</InputLabel>
-                  <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="(19)XXXXXXXX"
-                  />
-                </Stack>
-              </Grid>
-
-              <Grid item xs={12} md={12}>
-                <Stack spacing={1}>
-                  <InputLabel>CPF</InputLabel>
-                  <OutlinedInput
-
-                    id="outlined-required"
-                    defaultValue="XXX.XXX.XXX-XX"
-                  />
+                  {touched.cellphone && errors.cellphone && (
+                    <FormHelperText error id="helper-text-cellphone-signup">
+                      {errors.cellphone}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
 
@@ -112,45 +138,146 @@ const FormFields = () => {
                 <Stack spacing={1}>
                   <InputLabel>Rua</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="Rua Avenida Alameda"
+                    id='addressStreet'
+                    type='addressStreet'
+                    value={values.addressStreet}
+                    name='addressStreet'
+                    placeholder='Rua Alameda Avenida'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.addressStreet && errors.addressStreet)}
                   />
+                  {touched.addressStreet && errors.addressStreet && (
+                    <FormHelperText error id="helper-text-addressStreet-signup">
+                      {errors.addressStreet}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
+
               <Grid item xs={12} md={6}>
                 <Stack spacing={1}>
                   <InputLabel>Numero</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="15890"
+                    id='addressNumber'
+                    type='addressNumber'
+                    value={values.addressNumber}
+                    name='addressNumber'
+                    placeholder='523'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.addressNumber && errors.addressNumber)}
                   />
+                  {touched.addressNumber && errors.addressNumber && (
+                    <FormHelperText error id="helper-text-addressNumber-signup">
+                      {errors.addressNumber}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
-              <Grid item xs={12} md={6}>
-                <Stack spacing={1}>
-                  <InputLabel>CEP</InputLabel>
-                  <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="email@email.com"
-                  />
-                </Stack>
-              </Grid>
+
               <Grid item xs={12} md={6}>
                 <Stack spacing={1}>
                   <InputLabel>Bairro</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="email@email.com"
+                    id='addressNeighborhood'
+                    type='addressNeighborhood'
+                    value={values.addressNeighborhood}
+                    name='addressNeighborhood'
+                    placeholder='Centro'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.addressNeighborhood && errors.addressNeighborhood)}
                   />
+                  {touched.addressNeighborhood && errors.addressNeighborhood && (
+                    <FormHelperText error id="helper-text-addressNeighborhood-signup">
+                      {errors.addressNeighborhood}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
+
+              <Grid item xs={12} md={6}>
+                <Stack spacing={1}>
+                  <InputLabel>Cidade</InputLabel>
+                  <OutlinedInput
+                    id='addressCity'
+                    type='addressCity'
+                    value={values.addressCity}
+                    name='addressCity'
+                    placeholder='Campinas'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.addressCity && errors.addressCity)}
+                  />
+                  {touched.addressCity && errors.addressCity && (
+                    <FormHelperText error id="helper-text-addressCity-signup">
+                      {errors.addressCity}
+                    </FormHelperText>
+                  )}
+                </Stack>
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <Stack spacing={1}>
+                  <InputLabel>Estado</InputLabel>
+                  <OutlinedInput
+                    id='addressState'
+                    type='addressState'
+                    value={values.addressState}
+                    name='addressState'
+                    placeholder='São Paulo'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.addressState && errors.addressState)}
+                  />
+                  {touched.addressState && errors.addressState && (
+                    <FormHelperText error id="helper-text-addressState-signup">
+                      {errors.addressState}
+                    </FormHelperText>
+                  )}
+                </Stack>
+              </Grid>
+
+              <Grid item xs={12} md={6}>
+                <Stack spacing={1}>
+                  <InputLabel>CEP</InputLabel>
+                  <OutlinedInput
+                    id='addressZipcode'
+                    type='addressZipcode'
+                    value={values.addressZipcode}
+                    name='addressZipcode'
+                    placeholder='13820-000'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.addressZipcode && errors.addressZipcode)}
+                  />
+                  {touched.addressZipcode && errors.addressZipcode && (
+                    <FormHelperText error id="helper-text-addressZipcode-signup">
+                      {errors.addressZipcode}
+                    </FormHelperText>
+                  )}
+                </Stack>
+              </Grid>
+
               <Grid item xs={12} md={6}>
                 <Stack spacing={1}>
                   <InputLabel>Complemento</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="email@email.com"
+                    id='addressExtra'
+                    type='addressExtra'
+                    value={values.addressExtra}
+                    name='addressExtra'
+                    placeholder='Apartamento 15'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.addressExtra && errors.addressExtra)}
                   />
+                  {touched.addressExtra && errors.addressExtra && (
+                    <FormHelperText error id="helper-text-addressExtra-signup">
+                      {errors.addressExtra}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
 
@@ -162,9 +289,20 @@ const FormFields = () => {
                 <Stack spacing={1}>
                   <InputLabel>E-mail</InputLabel>
                   <OutlinedInput
-                    id="outlined-required"
-                    defaultValue="email@email.com"
+                    id='email'
+                    type='email'
+                    value={values.email}
+                    name='email'
+                    placeholder='email@email.com'
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    error={Boolean(touched.email && errors.email)}
                   />
+                  {touched.email && errors.email && (
+                    <FormHelperText error id="helper-text-email-signup">
+                      {errors.email}
+                    </FormHelperText>
+                  )}
                 </Stack>
               </Grid>
 
@@ -172,12 +310,54 @@ const FormFields = () => {
                 <Stack spacing={1}>
                   <InputLabel>Senha</InputLabel>
                   <OutlinedInput
-                    id="outlined-password-input"
-                    label="Senha"
-                    type="password"
-                    autoComplete="current-password"
+                    id='password'
+                    type={showPassword ? 'text' : 'password'}
+                    value={values.password}
+                    name='password'
+                    placeholder='******'
+                    onBlur={handleBlur}
+                    onChange={(e) => {
+                      handleChange(e);
+                    }}
+                    inputProps={{}}
+                    error={Boolean(touched.password && errors.password)}
+                    endAdornment={
+                      <InputAdornment position="end">
+                        <IconButton
+                          aria-label="toggle password visibility"
+                          onClick={handleClickShowPassword}
+                          edge="end"
+                          size="large"
+                        >
+                          {showPassword ? <VisibilityIcon /> : <VisibilityOffIcon />}
+                        </IconButton>
+                      </InputAdornment>
+                    }
                   />
+                  {touched.password && errors.password && (
+                    <FormHelperText error id="helper-text-password-signup">
+                      {errors.password}
+                    </FormHelperText>
+                  )}
                 </Stack>
+              </Grid>
+              {errors.submit && (
+                <Grid item xs={12}>
+                  <FormHelperText error>{errors.submit}</FormHelperText>
+                </Grid>
+              )}
+              <Grid item xs={12}>
+                  <Button
+                    disableElevation
+                    disabled={isSubmitting}
+                    fullWidth
+                    size="large"
+                    type="submit"
+                    variant="contained"
+                    color="primary"
+                  >
+                    Create Account
+                  </Button>
               </Grid>
 
 
