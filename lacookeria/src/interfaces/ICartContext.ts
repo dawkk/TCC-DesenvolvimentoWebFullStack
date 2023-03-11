017023a6e6e3b064ed99950aadffda15fc9348cc
dawkk <gventuri14@gmail.com>
2023-03-11 12:14:35 -0300

    Preparing for CI

--- lacookeria/src/interfaces/ICartContext.ts
+++
@@ -1,9 +0,0 @@
-import ICartItem from "./ICartItem";
-import IDish from "./IDish";
-
-export default interface ICartContext {
-  cartItems: ICartItem[];
-  addToCart: (item: IDish) => void;
-  removeFromCart: (item: IDish) => void;
-  clearCart: () => void;
-}
--- lacookeria/src/interfaces/IDish.ts
+++
@@ -1,9 +0,0 @@
-export default interface IDish {
-  _id: string,
-  title: string,
-  description: string,
-  price: number,
-  menu: string,
-  type: string
-}
-
