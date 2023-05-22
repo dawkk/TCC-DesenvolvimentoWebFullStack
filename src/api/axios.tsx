efec05618f1b551e4970b4ec19b6760ab8c67488
dawkk <gventuri14@gmail.com>
2023-05-22 14:27:04 -0300

    Solves axios problem

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const http = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_URL,
-  withCredentials: true
+ /*  withCredentials: true */
 });
 
 export default http;
